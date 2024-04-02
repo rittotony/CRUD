a572f1ca94f9213a917947ec87d4ac845144243f
Ritto Tony Mathew <110963847+rittotony@users.noreply.github.com>
2024-04-02 14:40:02 +0530

    Update connection.php

--- models/connection/connection.php
+++ models/connection/connection.php
@@ -4,7 +4,7 @@ class dbConnection
 {
 	function myDB()
 	{
-		$connection = mysqli_connect("localhost", "root", "#db-password", "#db-name");
+		$connection = mysqli_connect("localhost", "root", "", "lts");
 		if(!$connection)
 		{
 			Die("Failed to connect".mysqli_connect_error());
